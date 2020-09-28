2fbfaf6432ae4e29dc45156bc52a86db4a096515
Chelsea <chelsea.e.rolland@gmail.com>
2020-09-28 16:05:29 -0500

    added the java syntax, types, and exercise and completed it

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,98 @@ public class HelloWorld {
         System.out.print("Hello, ");
         System.out.print("World!");
         System.out.println();
+
+        //Q1
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        //Q2
+        String myString = "Fun-Times Ahead!";
+        System.out.println(myString);
+
+        //Q3
+        myString = "We really doing this!";
+        System.out.println(myString);
+
+        //Q4
+        //myString = 4
+        //It did not like since since it a number inside of string data type
+
+        //Q5
+//        long myNumber;
+//        System.out.print(myNumber);
+//        myNumber variable has not been initialized
+
+
+        //Q6
+//        long myNumber = 3.14;
+//        System.out.print(myNumber);
+//        3.14 is an incompatible number data type
+
+        //Q7
+//        long myNumber = 123L;
+//        System.out.print(myNumber);
+
+        //Q8
+//          long myNumber = 123;
+//        System.out.print(myNumber);
+
+        //Q9
+//        float myNumber = 3.14;
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+//        3.14 is an incompatible number data type from DOUBLE to FLOAT, need to add F at the end of it
+
+        //Q10
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        //Q11
+//        String class = "class";
+//        System.out.print(class);
+
+        //Q12
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+//        System.out.println(three);
+
+        //Q13
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x/= y;
+//        y -= x;
+//
+//        System.out.println(x);
+//        System.out.println(y);
+
+        //Q14
+        byte num = 125;
+        num += 5;
+
+        byte num1 = 127;
+        num1++;
+
+        System.out.println(num);
+        System.out.println(num1);
+
+
+
     }
 }
