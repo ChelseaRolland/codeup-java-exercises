d7bd488621607a3defbd4f35be5bb13a4203cd72
Chelsea <chelsea.e.rolland@gmail.com>
2020-09-29 16:17:24 -0500

    still working on the bonus

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -13,6 +13,7 @@ public class ConsoleExercises {
 //        int userInt = scan.nextInt();
 //
 //        System.out.println("Your number is: \"" + userInt + "\".");
+//        System.out.println("Your number is: %s.\n,userInt);
 //
 //        //Q3
 //        System.out.println("Enter 3 words: ");
@@ -21,7 +22,7 @@ public class ConsoleExercises {
 //        String thirdWord = scan.next();
 //
 //        System.out.printf("Your letters are: %s, %s, %s \n", firstWord, secondWord, thirdWord);
-
+//            scan.nextLine(); --> This captures the enter key that is pressed after the end of the 3rd next
         //Q4
         //System.out.println("Enter a sentence: ");
         //String yourSentence = scan.next();
@@ -36,22 +37,42 @@ public class ConsoleExercises {
 
         //Calculate the Perimeter and Area of the Codeup Class Room
 
-        //Q1
-        System.out.println("Enter the length and width of a CodeUP Classroom: ");
-        String userLength = scan.next();
-        String userWidth = scan.next();
-//        System.out.println(userLengthWidth);
-        int length = Integer.parseInt(userLength);
-        int width = Integer.parseInt(userWidth);
-        System.out.println("This is the length: " + length + ". This is the width: " + width + ".");
-//        System.out.println("The corresponding length and width of the classroom is: " + lengthWidth);
+//        //Q1
+//        System.out.println("Enter the length of a CodeUP Classroom: ");
+//        int length = Integer.parseInt(scan.nextLine());
+//
+//        System.out.println("Enter the width of a CodeUP Classroom: ");
+//        int width = Integer.parseInt(scan.nextLine());
+//
+//        System.out.println("This is the length: " + length + ". This is the width: " + width + ".");
+//
+//        //Q2
+//        int area = length * width;
+//        int perimeter = (2 * length) + (2 * width);
+//        System.out.printf("The area of the Codeup Classroom is: %d.\n", area);
+//        System.out.printf("The perimeter of the Codeup Classroom is: %d.\n", perimeter);
 
-        //Q2
-        System.out.println("The area of the Codeup Classroom is: " + length * width);
-        System.out.println("The perimeter of the Codeup Classroom is: " + ((length * 2) + (width * 2)));
+
+//        //BONUS
+//        System.out.println("Enter the length, width, and height of a CodeUP Classroom: ");
+//        int userLength = scan.nextInt();
+//        int userWidth = scan.nextInt();
+//        int userHeight = scan.nextInt();
+//
+//        //Accept Decimals
+//        double length = Double.parseDouble(userLength);
+//        double width = Double.parseDouble(userWidth);
+//        double height = Double.parseDouble(userHeight);
+//        System.out.println("This is the length: " + length + ". This is the width: " + width + " . This is the height: " + height + ".");
+//
+//        //Q2
+//        System.out.println("The perimeter of the Codeup Classroom is: " + ((length * 2) + (width * 2)));
+//        System.out.println("The area of the Codeup Classroom is: " + length * width);
+//        System.out.println("The volume of the Codeup Classroom is: " + length * width * height);
+//
+//        scan = scan.useDelimiter("\n");
 
 
-        //BONUS
 
 
     }
