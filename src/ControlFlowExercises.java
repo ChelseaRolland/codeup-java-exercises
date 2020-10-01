2c080cc86a7af12cb5550c8e0355224426e9a9cb
Chelsea <chelsea.e.rolland@gmail.com>
2020-10-01 11:53:37 -0500

    completed the while loop

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,19 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        //Loop Basics
+            //Q1: WHILE LOOPs
+            int i = 5;
+            while (i <= 15) {
+                System.out.println(i);
+                i++;
+            }
+
+            //Q2: DO-WHILE LOOPS
+            int j = 0;
+            do {
+
+            }
+
+
+    }
+}
