7be1e2e4e121a9186b5c533f5adadccf146c56b2
Chelsea <chelsea.e.rolland@gmail.com>
2020-10-02 12:12:13 -0500

    completed the review of the 1st string-basics exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class StringExercise {
     public static void main (String[] args) {
         String words = "We don't need no education";
@@ -9,11 +11,25 @@ public class StringExercise {
         words = words.replace("We don't need no thought control", "Check \"this\" out!, \"s inside of \"s!");
         System.out.println("words3: " + words);
 
-        words = words.replace("Check \"this\" out!, \"s inside of \"s!", "In windows, the main drive is usually C:");
-        words = words + "\\";
+        words = words.replace("Check \"this\" out!, \"s inside of \"s!", "In windows, the main drive is usually C:\\");
+        //words = words + "\\";
         System.out.println("words4: " + words);
 
-        words = words.replace("In windows, the main drive is usually C:\\", "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!");
+        words = words.replace("In windows, the main drive is usually C:\\", "I can do backslashes \\, double backslashes \\\\, \n" + "and the amazing triple backslash \\\\\\!");
         System.out.println("words5: " + words);
+
+        //Instructor Reviews
+        //Q1
+        System.out.print("We don't need no education\nWe don't need no thought control\n");
+
+        //Q2
+        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+
+        //Q3
+        System.out.println("In windows, the main drive is usually C:\\");
+
+        //Q4
+        System.out.println("I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!");
     }
 }
