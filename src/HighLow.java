aebc92e3b5f1be5a0279d3a10e427ab26f682dba
Chelsea <chelsea.e.rolland@gmail.com>
2020-10-05 13:50:33 -0500

    completed the review

--- src/HighLow.java
+++ src/HighLow.java
@@ -8,9 +8,9 @@ public class HighLow {
         guessGame();
     }
 
-    public static int randomNumber () {
-        int min = 1;
-        int max = 100;
+    public static int randomNumber (int min, int max) {
+        //int min = 1;
+        //int max = 100;
         int range = max - min + 1;
 
         int random = (int) (Math.random() * range) + min;
@@ -20,9 +20,10 @@ public class HighLow {
 
     public static int getInteger(int min, int max) {
         Scanner scan = new Scanner(System.in);
-        int userInput = scan.nextInt();
+        int userInput = Integer.parseInt(scan.nextLine());
         //setup the Conditional of the user input to determine if its outside the min and max range
         if (userInput < min || userInput > max) {
+            System.out.printf("Sorry the number you inputted is does not fit within the %d thru %d range!\n", min, max);
             System.out.printf("Please choose a number between %d and %d.\n", min, max);
             return getInteger(min, max);
         }
@@ -36,25 +37,24 @@ public class HighLow {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please chose a number between 1 and 100");
         int userNum = getInteger(1,100);
-        scan.nextLine();
-        int randomNumber = randomNumber();
+        int randomNumber = randomNumber(1,100);
 
         if (userNum == randomNumber){
-            System.out.println("Good Guess!");
+            System.out.println("GOOD GUESS!");
             System.out.printf("The random number was %d\n", randomNumber);
             System.out.println("Do you wish to continue? [y/n]");
             String userResponse = scan.nextLine();
             if (userResponse.equalsIgnoreCase("yes") || userResponse.equalsIgnoreCase("y")) {
-                guessGame();
+                return guessGame();
             }
-        } else if (userNum > randomNumber()) {
-            System.out.println("Lower...");
+        } else if (userNum > randomNumber) {
+            System.out.println("LOWER");
             System.out.printf("The random number was %d\n", randomNumber);
-            guessGame();
+            return guessGame();
         } else {
-            System.out.println("Higher...");
+            System.out.println("HIGHER");
             System.out.printf("The random number was %d\n", randomNumber);
-            guessGame();
+            return guessGame();
         }
 
 //        System.out.println("Do you wish to continue? [y/n]");
