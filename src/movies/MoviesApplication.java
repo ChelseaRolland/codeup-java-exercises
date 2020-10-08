c54b434dab54f0949ee7729bf57f24f4e2c6c8ca
Chelsea <chelsea.e.rolland@gmail.com>
2020-10-08 12:10:07 -0500

    still working on the movie app

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,8 +1,11 @@
 package movies;
 
+import jdk.jshell.ImportSnippet;
 import util.Input;
 
-import java.lang.reflect.Array;
+import java.util.Arrays;
+
+//import java.lang.reflect.Array;
 
 /** Movie List
 
@@ -20,22 +23,85 @@ import java.lang.reflect.Array;
  **/
 
 public class MoviesApplication {
+
+    static Movie[] movies = MoviesArray.findAll();
+
     public static void main(String[] args) {
         System.out.println("What would you like to do?");
 
-        System.out.println("0 - exit\n" +
+        System.out.println("\n" +
+                "0 - exit\n" +
                 "1 - view all movies\n" +
                 "2 - view movies in the animated category\n" +
                 "3 - view movies in the drama category\n" +
                 "4 - view movies in the horror category\n" +
                 "5 - view movies in the scifi category\n" +
                 "6 - view movies in the musical category\n" +
-                "7 - view movies in the comedy category");
+                "7 - view movies in the comedy category\n" +
+                "8 - ADD MOVIE\n");
 
         System.out.print("Enter your choice: ");
-        int userNum = new Input().getInt();
+        int userNum = new Input().getInt(0,8);
 
-        boolean welcomeToMovieApp = true;
+        boolean keepGoing = true;
+
+        //create a do-while loop to continue to ask the guest for movie options
+        do {
+            //menus are great for switch statements
+            switch (userNum) {
+                case 0:
+                    System.exit(0); //this will exit the system
+                    break;
+                case 1:
+                    System.out.println("View All Movies");
+                    for (Movie cinema : movies) {
+                        System.out.println(cinema.getMovieName() + " -- " + cinema.getMovieCategory());
+                    }
+                    break;
+                case 2:
+                    printMoviesByCategories("animated");
+                    break;
+                case 3:
+                    printMoviesByCategories("drama");
+                    break;
+                case 4:
+                    printMoviesByCategories("horror");
+                    break;
+                case 5:
+                    printMoviesByCategories("scifi");
+                    break;
+                case 6:
+                    printMoviesByCategories("musical");
+                    break;
+                case 7:
+                    printMoviesByCategories("comedy");
+                    break;
+                case 8:
+                    System.out.println("ADD Movie");
+                    String userMovieName = new Input().getString();
+                    String userMovieCategory = new Input().getString();
+
+                    addMovie(movies, new Movie(userMovieName, userMovieCategory));
+                    printMoviesByCategories(userMovieCategory);
+
+                    break;
+                default:
+                    System.out.println("Please input a valid number!");
+                    break;
+            }
+
+            System.out.println("Do want to chose another option? [y/n]");
+            String userResponse = new Input().getString();
+
+            if (!userResponse.equalsIgnoreCase("y")){
+                keepGoing = false;
+            }
+
+        } while(keepGoing);
+
+
+
+
 
         if (userNum == 1) {
             //View all of the movies
@@ -94,13 +160,20 @@ public class MoviesApplication {
             System.out.println("Please enter a valid movie option number!");
         }
 
-//        do {
-//            int counter = 0;
-//            counter++;
-//
-//
-//
-//        } while (welcomeToMovieApp);
+    }
 
+    public static Movie[] addMovie (Movie[] films, Movie obj) {
+        films = Arrays.copyOf(films, films.length+1);
+        films[films.length - 1] = obj;
+        return films;
+    }
+
+    public static void printMoviesByCategories(String aCategory) {
+        System.out.println("View All " + aCategory + "Movies");
+        for (Movie cinema : MoviesArray.findAll()) {
+            if ((cinema.getMovieCategory()).equalsIgnoreCase(aCategory)) {
+                System.out.println(cinema.getMovieName() + " -- " + cinema.getMovieCategory());
+            }
+        }
     }
 }
